e129ff077f517cde9d60a021ca460cf4a143edc9
saikumarchittaluri <127985962+saikumarchittaluri@users.noreply.github.com>
2023-08-22 15:28:39 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 //your JS code here. If required.
+// Get references to the necessary DOM elements
+    const sizeInfoDiv = document.getElementById('sizeInfo');
+    const h1Tag = sizeInfoDiv.querySelector('h1');
+
+    // Function to update the size information
+    function updateSizeInfo() {
+      const width = window.innerWidth;
+      const height = window.innerHeight;
+      h1Tag.textContent = `Width: ${width} Height: ${height}`;
+    }
+
+    // Initial update
+    updateSizeInfo();
+
+    // Attach an event listener to the window's resize event
+    window.addEventListener('resize', updateSizeInfo);
